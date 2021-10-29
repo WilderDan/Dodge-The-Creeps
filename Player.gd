447bc693d6ead3d3e378c3d875b3cd9804dafa0d
Dan Wilder <dwilder460@gmail.com>
2021-10-29 14:09:41 -0500

    Add mouse/touch support

--- Player.gd
+++ Player.gd
@@ -4,6 +4,9 @@ signal hit
 
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
+# Add this variable to hold the clicked position.
+var target = Vector2()
+var shouldMoveToTarget
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -11,12 +14,29 @@ func _ready():
 	
 func start(pos):
 	position = pos
+	# Initial target is the start position.
+	target = pos
+	shouldMoveToTarget = false
 	show()
 	$CollisionShape2D.disabled = false
 	
+# Change the target whenever a touch event happens.
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+		shouldMoveToTarget = true
+	
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
-
+	
+	# Mouse/Touch controls
+	if shouldMoveToTarget:
+		if position.distance_to(target) > 10:
+			velocity = target - position
+		else:
+			shouldMoveToTarget = false
+	
+	# Otherwise keyboard controls
 	if Input.is_action_pressed("ui_right"):
 	   velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
